833dd02a976676203445efbb33b624b2fa26cd73
Morpheus0208 <morpheus0208@gmail.com>
2025-07-04 10:55:13 +0200

    feat: routage du site

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
+/**
+ *  Represents the entry point of the Kasa application.
+ *
+ *  @author Guillaume Ramonet
+ *  @version 1.0.0
+ */
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router';
-import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import FicheLogement from './pages/FicheLogement';
+import About from './pages/About';
+import Error from './pages/Error';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/fiche-logement/:id" element={<FicheLogement />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   </StrictMode>
